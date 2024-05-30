d732e77dc517f469a476312471d8a8ae9e2cd0e8
1-21-01-E043-28\POO <john.cobacango@epn.edu.ec>
2024-05-30 08:54:49 -0500

    Set y get

--- src/Procesadores/Procesadores_AMD.java
+++ src/Procesadores/Procesadores_AMD.java
@@ -6,18 +6,13 @@ public class Procesadores_AMD extends Procesadores_Escritorio{
     public Procesadores_AMD() {
     }
 
-    public Procesadores_AMD(int nucleos, double consumo, double velocidad, String nombre) {
-        super(nucleos, consumo, velocidad, nombre);
-    }
-
-    public Procesadores_AMD(int nucleos, double consumo, double velocidad, String nombre, String soquet) {
+    public Procesadores_AMD(int nucleos, double consumo, double velocidad, String nombre, String soquet, boolean sensor_temperatura) {
         super(nucleos, consumo, velocidad, nombre, soquet);
+        this.sensor_temperatura = sensor_temperatura;
     }
-
-    public boolean isSensor_temperatura() {
+    public boolean getSensor_temperatura() {
         return sensor_temperatura;
     }
-
     public void setSensor_temperatura(boolean sensor_temperatura) {
         this.sensor_temperatura = sensor_temperatura;
     }
--- src/Procesadores/Procesadores_Intel.java
+++ src/Procesadores/Procesadores_Intel.java
@@ -3,4 +3,22 @@ package Procesadores;
 public class Procesadores_Intel extends Procesadores_Escritorio{
     boolean tarjeta_video;
 
+    public Procesadores_Intel() {
+    }
+
+    public Procesadores_Intel(int nucleos, double consumo, double velocidad, String nombre, String soquet, boolean tarjeta_video) {
+        super(nucleos, consumo, velocidad, nombre, soquet);
+        this.tarjeta_video = tarjeta_video;
+    }
+
+    public boolean getTarjeta_video() {
+        return tarjeta_video;
+    }
+
+    public void setTarjeta_video(boolean tarjeta_video) {
+        this.tarjeta_video = tarjeta_video;
+    }
+    public void imprimir_Intel(){
+        System.out.println("Nucleos: " + getNucleos());
+    }
 }
