84a86a0b350c5d7584c9feca2b380cbeee7bf59b
1-21-01-E043-27\Esf0t <richardpadilla3rod@gmail.com>
2024-05-30 09:17:54 -0500

    Integrantes

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,31 @@ public class Main {
         // Integrante uno Walter Cobacango
         Procesadores_AMD prec1 = new Procesadores_AMD(34, 23.55, 45.66, "WALT", "AA", true);
         Procesadores_AMD prec2 = new Procesadores_AMD();
-        prec1.imprimir_AMD();
+        prec2.imprimir_AMD();
 
 
 
-        //Integrante dos
-        Procesadores_Intel prec3 = new Procesadores_Intel();
+        //Richard Padilla
+        Procesadores_Intel prec3 = new Procesadores_Intel( );
         Procesadores_Intel prec4 = new Procesadores_Intel( );
+        prec3.imprimir_Intel();
         prec4.imprimir_Intel();
+
+        //Kevin Mendoza
+
+
+
+
+
+        //Ariel Sanches
+
+
+
+
+
+
+
+        //Marlon nicolalde
 
 
 
