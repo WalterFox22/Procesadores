47701fe2bc3e0470cbc5ff85cce08c9e8b92e9fb
1-21-01-E043-28\POO <john.cobacango@epn.edu.ec>
2024-05-30 07:51:55 -0500

    3

---
+++ src/Procesadores/Procesadores.java
@@ -0,0 +1,18 @@
+package Procesadores;
+
+public class Procesadores {
+    int nucleos;
+    double consumo, velocidad;
+    String nombre;
+
+    public Procesadores() {
+    }
+
+    public Procesadores(int nucleos, double consumo, double velocidad, String nombre) {
+        this.nucleos = nucleos;
+        this.consumo = consumo;
+        this.velocidad = velocidad;
+        this.nombre = nombre;
+    }
+
+}
