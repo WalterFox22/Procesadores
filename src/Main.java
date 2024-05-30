f8ca5dc1431ce31ed4135e9485bd4e545f069009
1-21-01-E043-28\POO <john.cobacango@epn.edu.ec>
2024-05-30 09:36:10 -0500

    amd 1

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,17 @@ public class Main {
 
         // Integrante uno Walter Cobacango 123
         Procesadores_AMD prec1 = new Procesadores_AMD(12, 105, 4.8, "AMD Ryzen 9 5900X", "AM4", true);
-        Procesadores_AMD prec2 = new Procesadores_AMD();
-        Procesadores_AMD prec3 = new Procesadores_AMD();
+        Procesadores_AMD prec2 = new Procesadores_AMD(8, 105, 4.7, "AMD Ryzen 7 5800X", "Socket AM4",true);
+        Procesadores_AMD prec3 = new Procesadores_AMD(6, 65, 4.6, "AMD Ryzen 5 5600X", "Socket AM4", true);
+        prec1.imprimir_AMD();
         prec2.imprimir_AMD();
+        prec3.imprimir_AMD();
 
 
 
         //Richard Padilla 4 5 6
-        Procesadores_Intel prec3 = new Procesadores_Intel( );
         Procesadores_Intel prec4 = new Procesadores_Intel( );
+        Procesadores_Intel prec = new Procesadores_Intel( );
         prec3.imprimir_Intel();
         prec4.imprimir_Intel();
 
