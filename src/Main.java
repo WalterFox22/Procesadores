ae15f7e542c5eb4d2c102b7e04a3c31634c8d89a
POO <kevin.mendoza01@epn.edu.ec>
2024-05-30 09:39:49 -0500

    kevinPRUEBA2

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,9 @@ public class Main {
         Procesadores_AMD prec5 = new Procesadores_AMD(12, 128,4400,"Ryzen 9 7900X3D","AM5",true);
         Procesadores_AMD prec6 = new Procesadores_AMD(8, 32, 3800, "AMD Ryzen 7 7700", "AM5", true );
         Procesadores_AMD prec7 = new Procesadores_AMD(8, 8, 3.3, "AMD FX-8300", "AM3+", true );
+        prec5.imprimir_AMD();
+        prec6.imprimir_AMD();
+        prec7.imprimir_AMD();
 
 
         //Ariel Sanches INTEL 10 11 12
