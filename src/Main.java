46d17576108abcc9502805cf94eef1d45dc3040d
1-21-01-E043-28\POO <john.cobacango@epn.edu.ec>
2024-05-30 09:25:30 -0500

    fdwef

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,27 @@ import Procesadores.Procesadores_Intel;
 public class Main {
     public static void main(String[] args) {
 
-        // Integrante uno Walter Cobacango
+        // Integrante uno Walter Cobacango 123
         Procesadores_AMD prec1 = new Procesadores_AMD(34, 23.55, 45.66, "WALT", "AA", true);
         Procesadores_AMD prec2 = new Procesadores_AMD();
-        Procese
+        Procesadores_AMD prec3 = new Procesadores_AMD();
         prec2.imprimir_AMD();
 
 
 
-        //Richard Padilla
+        //Richard Padilla 4 5 6
         Procesadores_Intel prec3 = new Procesadores_Intel( );
         Procesadores_Intel prec4 = new Procesadores_Intel( );
         prec3.imprimir_Intel();
         prec4.imprimir_Intel();
 
-        //Kevin Mendoza AMD
+        //Kevin Mendoza AMD 7 8 9
 
 
 
 
 
-        //Ariel Sanches INTEL
+        //Ariel Sanches INTEL 10 11 12
 
 
 
@@ -33,7 +33,7 @@ public class Main {
 
 
 
-        //Marlon nicolalde AMD
+        //Marlon nicolalde AMD 13 14 15
 
 
 
