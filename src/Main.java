c5fafc10515c42c9f86696a045177099a549cc53
Marlon Nicolalde <marlon.nicolalde@epn.edu.ec>
2024-05-30 09:40:25 -0500

    Objetos AMD listos

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,9 @@ public class Main {
 
 
         //Richard Padilla 4 5 6
-        Procesadores_Intel prec3 = new Procesadores_Intel( );
+        //Procesadores_Intel prec3 = new Procesadores_Intel( );
         Procesadores_Intel prec4 = new Procesadores_Intel( );
-        prec3.imprimir_Intel();
+        //prec3.imprimir_Intel();
         prec4.imprimir_Intel();
 
         //Kevin Mendoza AMD 7 8 9
@@ -34,7 +34,12 @@ public class Main {
 
 
         //Marlon nicolalde AMD 13 14 15
-
+        Procesadores_AMD prec13=new Procesadores_AMD(8, 3.56, 8.98, "Ryzen 5", "R5AMD", true);
+        Procesadores_AMD prec14=new Procesadores_AMD(12, 6.78, 9.67, "Ryzen 7", "R7AMD", false);
+        Procesadores_AMD prec15=new Procesadores_AMD(4, 2.45, 4.65, "Ryzen 3", "R3AMD", true);
+        prec13.imprimir_AMD();
+        prec14.imprimir_AMD();
+        prec15.imprimir_AMD();
 
 
 
